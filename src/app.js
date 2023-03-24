3099f2a932668aa20b6c3ba80a1121f12cdf17db
ParraJuanPablo <parrfjp@gmail.com>
2023-03-24 21:06:56 +0000

    My first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector(".card").classList.add(generatepinta());
+  document.querySelector(".card").innerHTML = generatenumber();
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generatenumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+let generatepinta = () => {
+  var pinta = ["diamond", "spade", "hearth", "club"];
+  let indexpinta = Math.floor(Math.random() * pinta.length);
+  return pinta[indexpinta];
 };
